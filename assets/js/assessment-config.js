e2ada603140d315a6ccccd57b8e4d042c7eb29ae
waghtuitionclasses <dharmstudytab@gmail.com>
2026-07-03 10:01:18 +0530

    Update assessment-config.js

--- assets/js/assessment-config.js
+++ assets/js/assessment-config.js
@@ -1,3 +1,3 @@
 const WTC_ASSESSMENT_CONFIG = {
-  API_URL: 'PASTE_WTC_AI_CONTENT_ENGINE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbzmnjLhsgnyfenpYI7vkih7Snk7FOymSj_Yp5ycU-4tP8QltaraYfAF_X8pj3DTwOeB2Q/exec'
 };

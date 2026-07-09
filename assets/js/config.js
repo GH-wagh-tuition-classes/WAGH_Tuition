e07650cf3b53aedccada03e69f3074ee9ce2a423
waghtuitionclasses <dharmstudytab@gmail.com>
2026-07-09 12:28:40 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,22 @@
 const WTC_CONFIG = {
   APP_NAME: 'WAGH Tuition Classes',
+
   API_URL: 'https://script.google.com/macros/s/AKfycbxVRRmp3rw9kPWlBNmS0px0Tce2HCEnU_epWJjnghooNJlxu5vdqJnKVuyKsRIDOEN0/exec',
+
   STORAGE_KEY: 'wtcUser',
   DEVICE_KEY: 'wtcDeviceId',
-  FALLBACK_CONTENT_URL: 'assets/data/fallback-content.json'
+
+  FALLBACK_CONTENT_URL: 'assets/data/fallback-content.json',
+
+  /* ===========================
+     Global Website Settings
+  =========================== */
+
+  BASE_URL: '',
+
+  LOGIN_PAGE: 'index.html#login',
+
+  WHATSAPP_NUMBER: '919537036383',
+
+  WHATSAPP_LINK: 'https://wa.me/919537036383'
 };
